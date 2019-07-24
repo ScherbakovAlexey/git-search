b25a95a46d18c28f7aea5c1d5dd65ec60482d408
scherbakovalexey <scherbakovalexey@mail.ru>
2019-07-24 11:57:25 +0300

    add redux, thunk

--- src/App.js
+++ src/App.js
@@ -3,68 +3,27 @@ import './App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
-import {debounce} from 'lodash';
-
+import {connect} from 'react-redux';
+import { asyncGetItems } from './actions/search';
 
 class App extends Component {
   constructor(props){
       super(props)
       this.state = {
-          list: []
       }
-      this.handleSearch = this.handleSearch.bind(this);
-  }
-
-  searchDebounce = debounce(query => {
-    let list = [];
-    if (query) fetch('https://api.github.com/search/repositories?q=' + query)
-    .then((res)=>{
-      res.json()
-        .then(response => {
-          //console.log(response.items);
-          if (response.items) {
-            if (response.items.length){
-              list = response.items.map((item) => {
-                return ({
-                   'name': item.name,
-                   'html_url': item.html_url,
-                   'stargazers_count': item.stargazers_count,
-                   'watchers_count': item.watchers_count
-                })
-              });
-            } else {
-              list[0] = {'name': 'Not found'};
-            }
-          } else if (response.message) {
-            list[0] = {'name': response.message};
-          }
-          //console.log(list);
-          this.setState({
-            list: list
-          });
-        })
-    })
-    .catch((err)=>{
-      list[0] = {'name': 'Something went wrong ', err};
-      this.setState({
-        list: list
-      });
-    })
-  }, 1000);
-
-  handleSearch(e){
-    const value = e.target.value.trim();
-    this.searchDebounce(value);
   }
 
   render (){
-    const listItems = this.state.list.map((item,index) => {
+    console.log('test: ', this.props.items);
+    const listItems = this.props.items.map((item,index) => {
       if (item.html_url){
         return (
           <li className="search__form__item"key={index}>
             <a href={item.html_url}>{item.name}</a>
-            <span className="icon"><FontAwesomeIcon icon={faEye} /> Watch {item.watchers_count}</span>
-            <span className="icon"><FontAwesomeIcon icon={faStar} /> Star {item.stargazers_count}</span>
+            <div>
+              <span className="icon"><FontAwesomeIcon icon={faEye} /> Watch {item.watchers_count}</span>
+              <span className="icon"><FontAwesomeIcon icon={faStar} /> Star {item.stargazers_count}</span>
+            </div>
           </li>
         )
       } else {
@@ -74,12 +33,11 @@ class App extends Component {
           </li>
         )
       }
-
     });
 
     return (
       <div className="search__form">
-        <input className="search__form__input" onChange={this.handleSearch} type="text" placeholder="Search a project ..."/>
+        <input className="search__form__input" onChange={this.props.onGetItems} type="text" placeholder="Search a project ..."/>
         <ul className="search__form__list">{listItems}</ul>
       </div>
     );
@@ -87,4 +45,15 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    items: state
+  }),
+  dispatch => ({
+    onGetItems:  (e) => {
+      let value = e.target.value;
+      console.log('value: ', value);
+      if (value.length > 2) dispatch(asyncGetItems(value));
+    }
+  })
+)(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import counter from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter, composeWithDevTools(applyMiddleware(thunk)));
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const render = () => ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
+
+render();
+store.subscribe(render);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
